f3eca98f98476cea63eb3889c9de7384ee660e93
ZafarUmarov <umarovzafar92@gmail.com>
2022-01-17 14:17:11 +0500

    project done

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const mobileMenu = document.querySelector("#mob__menu");
 const closeBtn = document.querySelector(".header__mob-close-btn")
 
 burgerBtn.addEventListener('click', () => {
+    console.log('bosl')
     mobileMenu.classList.toggle('active');
     burgerBtn.classList.toggle('falsy');
     closeBtn.classList.toggle("show__close-btn")
